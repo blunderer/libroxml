f1bd150127bab3c9b71a543f1ec753fa22626143
Tristan Lelong <tlelong@adeneo-embedded.com>
2012-11-21 17:58:35 -0800

    Fix export source for new debian package format

--- data/scripts/make_debian_source.sh
+++ data/scripts/make_debian_source.sh
@@ -10,11 +10,13 @@ VERSION=$1
 TOP_DIR=$(cd $(dirname $0)/../../ && pwd)
 TMP_DIR=/tmp/build-libroxml-$(date +"%y%m%d%H%M%S")
 
+LIBROXML_VERSION=$(sed -n "s,\(Version: \)\(.*\),\2,p" $TOP_DIR/libroxml.pc)
+
 mkdir -p $TMP_DIR
 
 echo "Exporting from GIT @$VERSION"
 (cd $TOP_DIR && git archive --format=tar --prefix=libroxml-$VERSION/ $VERSION . | gzip > $TMP_DIR/libroxml-$VERSION.tar.gz)
-tar zxf $TMP_DIR/libroxml-$VERSION.tar.gz -C $TMP_DIR/
+tar zxf $TMP_DIR/libroxml-$VERSION.tar.gz -C $TMP_DIR/ && rm -f $TMP_DIR/libroxml-$VERSION.tar.gz
 
 echo "Cleaning repository"
 rm -fr $TMP_DIR/libroxml-$VERSION/unittest
@@ -24,13 +26,18 @@ rm -fr $TMP_DIR/libroxml-$VERSION/TODO
 
 mv $TMP_DIR/libroxml-$VERSION/debian $TMP_DIR/
 
-echo "Generate original package"
-cd $TMP_DIR && tar zcf libroxml_$VERSION.orig.tar.gz libroxml-$VERSION> /dev/null
+echo "Export native source package"
+cd $TMP_DIR
+mv libroxml-$VERSION libroxml-$LIBROXML_VERSION 
+tar zcf libroxml-$LIBROXML_VERSION.tar.gz libroxml-$LIBROXML_VERSION > /dev/null
+mv libroxml-$LIBROXML_VERSION libroxml_$LIBROXML_VERSION 
 
-mv $TMP_DIR/debian $TMP_DIR/libroxml-$VERSION/
+echo "Generate original package"
+tar zcf libroxml_$LIBROXML_VERSION.orig.tar.gz libroxml_$LIBROXML_VERSION > /dev/null
 
 echo "Build package"
-cd $TMP_DIR/libroxml-$VERSION/ && dpkg-buildpackage
+mv $TMP_DIR/debian $TMP_DIR/libroxml_$LIBROXML_VERSION/
+cd $TMP_DIR/libroxml_$LIBROXML_VERSION/ && dpkg-buildpackage
 
 echo "********************************************************************"
 echo "Your package is built in '$TMP_DIR'"
