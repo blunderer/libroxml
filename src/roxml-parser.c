f59df7fc642c813b3ff2c4aad18ce875d8fd6f02
Tristan Lelong <tristan.lelong@blunderer.org>
2013-07-16 09:45:38 -0700

    Update libroxml license.
    Add a static linking exception for libroxml to solve the case of Android
    and iOS packages that cannot easily comply with LGPL.

--- src/roxml-parser.c
+++ src/roxml-parser.c
@@ -10,6 +10,7 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
+ * The author added a static linking exception, see License.txt.
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
