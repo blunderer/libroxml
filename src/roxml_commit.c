c7fc439a99c01548a5673b7c9a8be82334f10972
Tristan Lelong <tristan.lelong@blunderer.org>
2015-09-21 22:52:02 -0700

    roxml_commit: Fix compilation warning on implicit dup() declaration

--- src/roxml_commit.c
+++ src/roxml_commit.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include "roxml_core.h"
 
 ROXML_STATIC ROXML_INT void roxml_realloc_buf(char **buf, int *len, int min_len)
