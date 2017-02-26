0568f80e41e8c99999130e307b677f5499460468
Tristan Lelong <tristan@lelong.xyz>
2017-02-26 12:06:15 +0000

    roxml_shell: Add subtree display.
    Using the -c command line option, one can dump an XML subtree identified by xpath expression.
    Without any xpath command line parameter, this will dump the whole file.
    
    Signed-off-by: Tristan Lelong <tristan@lelong.xyz>

--- src/roxml_shell.c
+++ src/roxml_shell.c
@@ -18,19 +18,22 @@
 
 void print_help(void)
 {
-	fprintf(stderr, "LGPL command line XML parser\n<blunderer@blunderer.org>\n");
+	fprintf(stderr, "LGPL command line XML parser\n<tristan.lelong@libroxml.net>\n");
 }
 
 void print_usage(const char *progname)
 {
 	fprintf(stderr, "\nusage: %s [-q|-h] <filename> [/]<node1>/<node2>/<node3>/.../<nodeN>\n", progname);
-	fprintf(stderr, "-q|--quiet activate quiet mode\n-h|--help display this message\n");
+	fprintf(stderr, "-q|--quiet: activate quiet mode\n");
+	fprintf(stderr, "-c|--content: show content of node instead of name\n");
+	fprintf(stderr, "-h|--help: display this message\n");
 }
 
 int main(int argc, char **argv)
 {
 	int optind;
 	int quiet = 0;
+	int content = 0;
 	int j, max;
 	node_t *root;
 	node_t **ans;
@@ -38,21 +41,18 @@ int main(int argc, char **argv)
 	for (optind = 1; optind < argc; optind++) {
 		int option = 0;
 		if (argv[optind][0] == '-') {
-			/* this is an option */
-			if (argv[optind][1] == '-') {
-				/* long option */
-				if (strcmp(argv[optind], "--help") == 0) {
-					option = 'h';
-				} else if (strcmp(argv[optind], "--quiet") == 0) {
-					option = 'q';
-				}
-			} else {
-				/* short option */
-				if (strcmp(argv[optind], "-h") == 0) {
-					option = 'h';
-				} else if (strcmp(argv[optind], "-q") == 0) {
-					option = 'q';
-				}
+			if (strcmp(argv[optind], "--help") == 0) {
+				option = 'h';
+			} else if (strcmp(argv[optind], "--content") == 0) {
+				option = 'c';
+			} else if (strcmp(argv[optind], "--quiet") == 0) {
+				option = 'q';
+			} else if (strcmp(argv[optind], "-h") == 0) {
+				option = 'h';
+			} else if (strcmp(argv[optind], "-c") == 0) {
+				option = 'c';
+			} else if (strcmp(argv[optind], "-q") == 0) {
+				option = 'q';
 			}
 		} else {
 			break;
@@ -60,6 +60,9 @@ int main(int argc, char **argv)
 		switch (option) {
 		case 'q':
 			quiet = 1;
+			break;
+		case 'c':
+			content = 1;
 			break;
 		case 'h':
 			print_help();
@@ -73,48 +76,52 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (argc < optind + 2) {
-		fprintf(stderr, "wrong syntax\n");
-		print_usage(argv[0]);
-		return -1;
-	}
-
 	root = roxml_load_doc(argv[optind]);
 	if (root == NULL) {
-		perror("error parsing xml file");
+		if (!quiet)
+			perror("error parsing xml file");
 		goto error;
 	}
 
-	ans = roxml_xpath(root, argv[optind + 1], &max);
+	if (argc < optind + 2)
+		ans = roxml_xpath(root, "/", &max);
+	else
+		ans = roxml_xpath(root, argv[optind + 1], &max);
 
 	for (j = 0; j < max; j++) {
 		char *c = NULL;
 
-		c = roxml_get_content(ans[j], NULL, 0, NULL);
-		if (c) {
-			if (*c == 0) {
-				char *s = NULL;
-				int i = 0;
-				int nb_chld = roxml_get_chld_nb(ans[j]);
+		// Dump the XML subtree.
+		if (content) {
+			roxml_commit_fd(ans[j], 1, 1);
+			fprintf(stdout, "\n");
+			fflush(stdout);
+			continue;
+		}
 
-				for (i = 0; i < nb_chld; i++) {
-					node_t *child = roxml_get_chld(ans[j], NULL, i);
-					node_t *ns = roxml_get_ns(child);
-					c = roxml_get_name(child, NULL, 0);
-					if (!quiet) {
-						fprintf(stdout, "ans[%d]: ", j);
-					}
-					if (ns) {
-						s = roxml_get_name(ns, NULL, 0);
-						if (strlen(s)) {
-							fprintf(stdout, "%s:", s);
-						}
-					}
-					fprintf(stdout, "%s\n", c);
-				}
-			} else {
-				if (!quiet) {
+		// Or display:
+		// - the node content for leaf nodes.
+		// - list of children otherwise. 
+		c = roxml_get_content(ans[j], NULL, 0, NULL);
+		if (c && *c) {
+			if (!quiet)
+				fprintf(stdout, "ans[%d]: ", j);
+			fprintf(stdout, "%s\n", c);
+		} else if (c) {
+			char *s = NULL;
+			int nb_chld = roxml_get_chld_nb(ans[j]);
+			int i = 0;
+
+			for (i = 0; i < nb_chld; i++) {
+				node_t *child = roxml_get_chld(ans[j], NULL, i);
+				node_t *ns = roxml_get_ns(child);
+				c = roxml_get_name(child, NULL, 0);
+				if (!quiet)
 					fprintf(stdout, "ans[%d]: ", j);
+				if (ns) {
+					s = roxml_get_name(ns, NULL, 0);
+					if (strlen(s))
+						fprintf(stdout, "%s:", s);
 				}
 				fprintf(stdout, "%s\n", c);
 			}
